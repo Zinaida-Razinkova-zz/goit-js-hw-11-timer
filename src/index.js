1afe9d7a5ffa61818d1cd0b0abb9b506fdcaac31
Zinaida Razinkova <razinkova.zina@gmail.com>
2021-01-22 10:54:40 +0200

    Добавляет ДЗ

---
+++ src/index.js
@@ -0,0 +1,114 @@
+// Timer через класс
+const refs = {
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]'),
+  timerFace: document.getElementById('timer-1'),
+};
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+
+  start() {
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      this.updateTimer(0);
+
+      const deltaTime = this.targetDate - currentTime;
+      this.updateTimer(deltaTime);
+
+      if (deltaTime <= 0) {
+        this.stop();
+      }
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+    this.updateTimer(0);
+  }
+
+  updateTimer(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    refs.days.textContent = `${days}`;
+    refs.hours.textContent = `${hours}`;
+    refs.mins.textContent = `${mins}`;
+    refs.secs.textContent = `${secs}`;
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+}
+
+const Timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date(2021, 2, 22, 0, 0, 0),
+});
+
+// Начало работы таймера
+Timer.start();
+
+// Timer
+// const refs = {
+//   days: document.querySelector('[data-value="days"]'),
+//   hours: document.querySelector('[data-value="hours"]'),
+//   mins: document.querySelector('[data-value="mins"]'),
+//   secs: document.querySelector('[data-value="secs"]'),
+//   timerFace: document.getElementById('timer-1'),
+// };
+
+// const timer = {
+//   intervalId: null,
+//   start() {
+//     this.intervalId = setInterval(() => {
+//       const currentTime = Date.now();
+//       const targetDate = new Date(2021, 2, 22, 0, 0, 0);
+//       updateTimer(0);
+
+//       const deltaTime = targetDate - currentTime;
+//       updateTimer(deltaTime);
+
+//       if (deltaTime <= 0) {
+//         this.stop();
+//       }
+//     }, 1000);
+//   },
+//   stop() {
+//     clearInterval(this.intervalId);
+//     this.intervalId = null;
+//     updateTimer(0);
+//   },
+// };
+
+// // Начало работы таймера
+// timer.start();
+
+// function updateTimer(time) {
+//   const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+//   const hours = pad(
+//     Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+//   );
+//   const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+//   const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+//   refs.days.textContent = `${days}`;
+//   refs.hours.textContent = `${hours}`;
+//   refs.mins.textContent = `${mins}`;
+//   refs.secs.textContent = `${secs}`;
+// }
+
+// function pad(value) {
+//   return String(value).padStart(2, '0');
+// }
